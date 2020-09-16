d45eccc6563374b293c3025839146f79aae1bd0c
Hikvineh <rarifky.aditya@gmail.com>
2020-09-17 00:23:12 +0700

    bugfix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 import FetchingData from './controllers/FetchingData';
+import express from 'express';
+
+const app = express();
+const { PORT = 3000 } = process.env;
 
 console.log("---------------------");
 console.log("Running Job");
 FetchingData.fetchData;
 console.log("Done");
 console.log("---------------------");
+
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
