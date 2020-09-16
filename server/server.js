33426e00e2a0f9f3cf836d833d93690a08740f21
Hikvineh <rarifky.aditya@gmail.com>
2020-09-17 00:30:27 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
-import FetchingData from './controllers/FetchingData';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import Redis from 'ioredis';
+import JSONCache from 'redis-json';
 import express from 'express';
+
+dotenv.config();
+
+let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+
+const redis = new Redis(REDIS_URL);
+
+const jsonCache = new JSONCache(redis)
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 console.log("---------------------");
 console.log("Running Job");
-FetchingData.fetchData;
+
+let response = await fetch(`https://api.covid19api.com/summary`);
+const data = await response.json();
+const result = JSON.stringify(data);
+await jsonCache.set('data', result);
+console.log("Get Data");
+await jsonCache.get('data');
+
 console.log("Done");
 console.log("---------------------");
 
