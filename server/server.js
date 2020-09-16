f3bf1af543bfd690bfc3d9b74d021a0d7d5d76fc
Hikvineh <rarifky.aditya@gmail.com>
2020-09-16 23:38:08 +0700

    remove express

--- server/server.js
+++ server/server.js
@@ -1,11 +1,5 @@
 import FetchingData from './controllers/FetchingData';
-import express from 'express';
-
-const app = express();
-const { PORT = 3000 } = process.env;
 
 console.log("---------------------");
 console.log("Running Job");
 FetchingData.fetchData();
-
-app.listen(PORT, () => console.log(`App Listening on port ${PORT}`));
